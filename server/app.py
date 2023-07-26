3b7675819215d605258f8e341bccd1f92bbf7362
TJ Stifter <tjstifter@gmail.com>
2023-07-26 10:04:17 -0400

    passes test

--- server/app.py
+++ server/app.py
@@ -17,11 +17,40 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        all_plants = Plant.query.all()
 
-class PlantByID(Resource):
-    pass
+        res_dict = [plant.to_dict() for plant in all_plants]
+        response = make_response(jsonify(res_dict), 200)
         
+        return response
+
+    def post(self):
+        json_data = request.get_json()
+        new_plant = Plant(
+            name = json_data['name'].strip(),
+            image = json_data['image'].strip(),
+            price = json_data['price'],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+
+        np_dict = new_plant.to_dict()
+        response = make_response(jsonify(np_dict), 201)
+
+        return response
+
+api.add_resource(Plants, '/plants')
+class PlantByID(Resource):
+    def get(self, id):
+
+        resp_d = Plant.query.filter(Plant.id == id).first().to_dict()
+
+        response = make_response(jsonify(resp_d), 200)
+
+        return response
+        
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -5,5 +5,8 @@ db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
-
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
